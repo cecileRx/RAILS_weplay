41b14efbf41812db053baa9daf90a75a57421aea
cecile rougnaux <cecile_rougnaux@hotmail.com>
2022-04-17 21:35:37 +0200

    basic calendar with monthly events

---
+++ app/controllers/events_controller.rb
@@ -0,0 +1,18 @@
+class EventsController < ApplicationController
+  def index
+    @events = Event.all
+  end
+
+  def new
+    @event = Event.new
+  end
+
+  def create
+    Event.create(event_parameter)
+    redirect_to root_path
+  end
+
+  def event_parameter
+    params.require(:event).permit(:title, :content, :start_time)
+  end
+end
