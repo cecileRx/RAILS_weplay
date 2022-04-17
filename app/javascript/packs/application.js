9464bcddcb6f05d4a0dc54919f9d8f0e7cb3b8f2
cecile rougnaux <cecile_rougnaux@hotmail.com>
2022-04-17 22:00:47 +0200

    bootstrap added

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,7 +7,25 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
+require("@popperjs/core")
+import "bootstrap"
+import { Tooltip, Popover } from "bootstrap"
+require("../stylesheets/application.scss")
+
 
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
+
+document.addEventListener("turbolinks:load", () => {
+    // Both of these are from the Bootstrap 5 docs
+    var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+    var tooltipList = tooltipTriggerList.map(function(tooltipTriggerEl) {
+        return new Tooltip(tooltipTriggerEl)
+    })
+
+    var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
+    var popoverList = popoverTriggerList.map(function(popoverTriggerEl) {
+        return new Popover(popoverTriggerEl)
+    })
+})
--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,9 @@
 const { environment } = require('@rails/webpacker')
+const webpack = require("webpack")
 
+environment.plugins.append("Provide", new webpack.ProvidePlugin({
+    $: 'jquery',
+    jQuery: 'jquery',
+    Popper: ['popper.js', 'default']  // we're still using popper.js here but @popper/js/core is also implemented to work properly with Bootstrap 5
+}))
 module.exports = environment
